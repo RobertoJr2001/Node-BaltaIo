f1ce75ad1bf97fc483bb4b402fd1c891465137d2
Roberto Jr <junionascimento2001@gmail.com>
2020-05-15 14:10:16 -0300

    _

--- src/app.js
+++ src/app.js
@@ -3,25 +3,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
-// DataBase
-mongoose.connect('Connection String');
+// DATABASE
+mongoose.connect(config.connectionString);
 
 // Models
-const Product = require('./model/product'); 
+const Product = require('./model/product');
+const Customer = require('./model/customer');
+const Order = require('./model/order');
 
 // Routes
-const indexRoutes = require('./routes/index');
-const products = require('./routes/products');
+const indexRoute = require('./routes/index');
+const productRoute = require('./routes/product');
+const customerRoute = require('./routes/customer');
+const orderRoute = require('./routes/order');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({
+  limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
-app.use('/', indexRoutes);
-app.use('/products', products);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
 
 module.exports = app;
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,98 +1,126 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const Product = mongoose.model('Product');
+const ValidationContract = require('../validate/validator');
+const repository = require('../repositories/productRepository');
+const azure = require('azure-storage');
+const guid = require('guid');
+var config = require('../config');
 
-exports.get = (req, res, next) => {
-  Product.find({ 
-    active: true }, 
-    'title price slug'
-    )
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-    });;
-};
-
-exports.getBySlug = (req, res, next) => {
-  Product.findOne({ slug: req.params.slug, 
-    active: true }, 
-    'title description price slug tags'
-    )
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-    });;
-};
-
-exports.getById = (req, res, next) => {
-  Product.findById(req.params.id)
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-    });;
-};
-
-exports.getByTag = (req, res, next) => {
-  Product.find({ 
-    tags: req.params.tag, 
-    active: true}, 
-    'title description price slug tags'
-    )
-    .then(data => {
-      res.status(200).send(data);
-    }).catch(e => {
-      res.status(400).send(e);
-    });;
-};
-
-exports.post = (req, res, next) => {
-  let product = new Product(req.body);
-  product.save()
-    .then(x => {
-      res.status(201).send({ 
-        message: 'Produto cadastrado com sucesso!' 
-      });
-    }).catch(e => {
-      res.status(400).send({ 
-        message: 'Falha ao cadastrar o produto', 
-        data: e, 
-      });
-    });
-};
-
-exports.put = (req, res, next) => {
-  Product.findByIdAndUpdate(req.params.id, {
-    $set: {
-      title: req.body.title,
-      description: req.body.params.description,
-      price: req.params.price,
-      slug: req.body.slug,
+exports.get = async(req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
     }
-  }).then(x => {
-    res.status(200).send({ 
-      message: 'Produto atualizado com sucesso!' 
+}
+
+exports.getBySlug = async(req, res, next) => {
+    try {
+        var data = await repository.getBySlug(req.params.slug);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.getById = async(req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.getByTag = async(req, res, next) => {
+    try {
+        const data = await repository.getByTag(req.params.tag);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.post = async(req, res, next) => {
+  let contract = new ValidationContract();
+  contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+  contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
+  contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteres');
+  
+  // Se os dados forem inválidos
+  if (!contract.isValid()) {
+    res.status(400).send(contract.errors()).end();
+    return;
+  }
+  
+  try {
+    // Cria o Blob Service
+    // const blobSvc = azure.createBlobService(config.containerConnectionString);
+    let filename = guid.raw().toString() + '.jpg';
+    let rawdata = req.body.image;
+    let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    let type = matches[1];
+    let buffer = new Buffer(matches[2], 'base64');
+    // Salva a imagem
+    await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
+      contentType: type
+    }, function (error, result, response) {
+      if (error) {
+        filename = 'default-product.png'
+      }
     });
-  }).catch(e => {
-    res.status(400).send({ 
-      message: 'Falha ao atualizar o produto', 
-      data: e, 
+    await repository.create({
+      title: req.body.title,
+      slug: req.body.slug,
+      description: req.body.description,
+      price: req.body.price,
+      active: true,
+      tags: req.body.tags,
+      image: 'https://nodestr.blob.core.windows.net/product-images/' + filename
     });
-  });
+    res.status(201).send({
+      message: 'Produto cadastrado com sucesso!'
+    });
+
+  } catch (e) {
+      console.log(e);
+      res.status(500).send({
+          message: 'Falha ao processar sua requisição'
+      });
+  }
 };
 
-exports.delete = (req, res, next) => {
-  Product.findOneAndRemove(req.params.id).then(x => {
-    res.status(200).send({ 
-      message: 'Produto removido com sucesso!' 
+exports.put = async(req, res, next) => {
+  try {
+    await repository.update(req.params.id, req.body);
+    res.status(200).send({
+      message: 'Produto atualizado com sucesso!'
     });
-  }).catch(e => {
-    res.status(400).send({ 
-      message: 'Falha ao remover o produto', 
-      data: e, 
+  } catch (e) {
+    res.status(500).send({
+       message: 'Falha ao processar sua requisição'
     });
-  });
+  }
+};
+
+exports.delete = async(req, res, next) => {
+  try {
+    await repository.delete(req.body.id)
+    res.status(200).send({
+      message: 'Produto removido com sucesso!'
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
 };
